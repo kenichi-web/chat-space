d066350a0a152153cd4729ec276c3e316ee36169
kenichi-web <ttnjg0808@ezweb.ne.jp>
2019-10-25 15:24:18 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -43,7 +43,6 @@ $('.new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
-//  console.log(ok)
  $.ajax({
    url: url,
    type: "POST",
